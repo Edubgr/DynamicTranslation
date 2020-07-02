c6f63e3fe4e7fee5fdf18e38bc34dc3bb17c5c56
Eduardo <eduguibar@gmail.com>
2020-07-02 16:04:56 -0300

    Hide APY_KEY

--- routes/translation.js
+++ routes/translation.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var config = require("../config")
 const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
 const { IamAuthenticator } = require('ibm-watson/auth');
 var router = express.Router();
@@ -6,9 +7,9 @@ var router = express.Router();
 const languageTranslator = new LanguageTranslatorV3({
   version: '2019-04-04',
   authenticator: new IamAuthenticator({
-    apikey: '_lJFjh24vGapEXbtjdGFbY7D0cpkee72qtBkJ9zXzP5T',
+    apikey: config.API_KEY,
   }),
-  url: 'https://api.us-south.language-translator.watson.cloud.ibm.com/instances/d65aadcc-e999-434d-9153-f21c8504f544',
+  url: config.Url,
 });
 
 router.get('/tradutor/:strin', function(req, res, next) {
